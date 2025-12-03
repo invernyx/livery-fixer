3ead1590d8ee2226bfef216540c62de844d71b2e
Collin Biedenkapp <turbofandude@gmail.com>
2025-12-03 17:42:29 -0500

    feat: better structure

--- liveryfixer/Program.cs
+++ liveryfixer/Program.cs
@@ -11,6 +11,105 @@ namespace liveryfixer
 {
     internal class Program
     {
+        static List<string> CheckRegistrations(ref List<LiveryPackage> packages)
+        {
+            List<string> errors = new List<string>();
+
+            List<string> registrations = new List<string>();
+            foreach (LiveryPackage pkg in packages)
+            {
+                foreach (LiveryGroup group in pkg.groups)
+                {
+                    foreach (Livery livery in group.Liveries)
+                    {
+                        string reg = livery.Registration.ToLowerInvariant().Replace("\"", "");
+                        if (!registrations.Contains(reg))
+                            registrations.Add(reg);
+                        else if(string.IsNullOrEmpty(reg))
+                            errors.Add($"Missing registration in {livery.Path}");
+                        else
+                            errors.Add($"Duplicate registration found: {reg} in {livery.Path}");
+                    }
+                }
+            }
+
+            return errors;
+        }
+
+        static Dictionary<string, List<string>> ListTypes(ref List<LiveryPackage> packages)
+        {
+            Dictionary<string, List<string>> types = new Dictionary<string, List<string>>();
+            foreach (LiveryPackage pkg in packages)
+            {
+                foreach (LiveryGroup group in pkg.groups)
+                {
+                    foreach (Livery livery in group.Liveries)
+                    {
+                        if (!types.ContainsKey(livery.Type))
+                            types[livery.Type] = new List<string>();
+                        types[livery.Type].Add(livery.Path);
+                    }
+                }
+            }
+            return types;
+        }
+
+        static List<string> VerifyICAOs(ref List<LiveryPackage> packages)
+        {
+            List<string> errors = new List<string>();
+
+            Console.WriteLine("============Missing Airline ICAO============");
+            foreach (LiveryPackage pkg in packages)
+            {
+                foreach (LiveryGroup group in pkg.groups)
+                {
+                    foreach (Livery livery in group.Liveries)
+                    {
+                        if (string.IsNullOrEmpty(livery.AirlineICAO?.Replace("\"", "")))
+                        {
+                            errors.Add($"Missing Airline ICAO for registration: {livery.Registration} in {livery.Path}");
+                        }
+                    }
+                }
+            }
+
+            return errors;
+        }
+
+        static List<string> RenameFolders(ref List<LiveryPackage> packages)
+        {
+            List<string> actionsTaken = new List<string>();
+
+            foreach (LiveryPackage pkg in packages)
+            {
+                string desName = Options.packagePrefix;
+                if (pkg.groups.Count > 1)
+                {
+                    desName += string.Join(" - ", (pkg.groups.SelectMany(g => g.Liveries).Select(l => l.Type.Replace("\"", ""))).Distinct());
+                    desName += "-" + string.Join(" - ", (pkg.groups.SelectMany(g => g.Liveries).Select(l => l.AirlineICAO.Replace("\"", ""))).Distinct());
+                    desName += "-pack";
+                }
+                else
+                {
+                    if (pkg.groups[0].Liveries.Count > 1)
+                    {
+                        desName += pkg.groups[0].Liveries[0].Type.Replace("\"", "") + " - ";
+                        desName += string.Join(" - ", pkg.groups[0].Liveries.SelectMany(l => l.AirlineICAO.Replace("\"", "")).Distinct());
+                        desName += "-pack";
+                    }
+                    else
+                    {
+                        string icao = pkg.groups[0].Liveries[0].AirlineICAO.Replace("\"", "");
+                        desName += pkg.groups[0].Liveries[0].Type.Replace("\"", "") + " - " + (icao.Length > 0 ? icao + "-" : "") + pkg.groups[0].Liveries[0].Registration.Replace("\"", "");
+                    }
+                }
+
+                desName = desName.Replace(" ", "").ToLowerInvariant();
+            }
+
+            return actionsTaken;
+        }
+
         static void Main(string[] args)
         {
             try
@@ -171,82 +270,51 @@ namespace liveryfixer
                                     packages.Add(package);
                             }
 
-                            //Renaming Package Directories
-                            foreach (LiveryPackage pkg in packages)
+                            
+                            List<string> regErrors = CheckRegistrations(ref packages);
+                            if(regErrors.Count > 0)
                             {
-                                string desName = Options.packagePrefix;
-                                if (pkg.groups.Count > 1)
+                                Console.WriteLine("============Registration Errors============");
+                                foreach (string err in regErrors)
                                 {
-                                    desName += string.Join(" - ", (pkg.groups.SelectMany(g => g.Liveries).Select(l => l.Type.Replace("\"", ""))).Distinct());
-                                    desName += "-" + string.Join(" - ", (pkg.groups.SelectMany(g => g.Liveries).Select(l => l.AirlineICAO.Replace("\"", ""))).Distinct());
-                                    desName += "-pack";
+                                    Console.WriteLine("\t" + err);
                                 }
-                                else
-                                {
-                                    if (pkg.groups[0].Liveries.Count > 1)
-                                    {
-                                        desName += pkg.groups[0].Liveries[0].Type.Replace("\"", "") + " - ";
-                                        desName += string.Join(" - ", pkg.groups[0].Liveries.SelectMany(l => l.AirlineICAO.Replace("\"", "")).Distinct());
-                                        desName += "-pack";
-                                    }
-                                    else
-                                    {
-                                        string icao = pkg.groups[0].Liveries[0].AirlineICAO.Replace("\"", "");                                        
-                                        desName += pkg.groups[0].Liveries[0].Type.Replace("\"", "") + " - " + (icao.Length > 0 ? icao + "-" : "") +  pkg.groups[0].Liveries[0].Registration.Replace("\"", "");
-                                    }
-                                }
-
-                                desName = desName.Replace(" ", "").ToLowerInvariant();
                             }
 
-                            //Verification
-                            Dictionary<string, List<string>> types = new Dictionary<string, List<string>>();
-                            List<string> registrations = new List<string>();
-
-                            Console.WriteLine("============Missing Airline ICAO============");
-                            foreach (LiveryPackage pkg in packages)
+                            Dictionary<string, List<string>> types = ListTypes(ref packages);
+                            if(types.Count > 0)
                             {
-                                foreach (LiveryGroup group in pkg.groups)
+                                Console.WriteLine("============Livery Types============");
+                                foreach (var type in types)
                                 {
-                                    foreach (Livery livery in group.Liveries)
+                                    Console.WriteLine($"\tType: {type.Key}");
+                                    foreach (string path in type.Value)
                                     {
-                                        if (string.IsNullOrEmpty(livery.AirlineICAO?.Replace("\"", "")))
-                                        {
-                                            Console.WriteLine($"\tMissing Airline ICAO for registration: {livery.Registration} in {livery.Path}");
-                                        }
+                                        Console.WriteLine($"\t\t{path}");
                                     }
                                 }
                             }
 
-                            Console.WriteLine("============Registrations============");
-                            foreach (LiveryPackage pkg in packages)
+                            List<string> icaoData = VerifyICAOs(ref packages);
+                            if (icaoData.Count > 0)
                             {
-                                foreach (LiveryGroup group in pkg.groups)
+                                Console.WriteLine("============ICAO Errors============");
+                                foreach (string err in icaoData)
                                 {
-                                    foreach (Livery livery in group.Liveries)
-                                    {
-                                        if (!types.ContainsKey(livery.Type))
-                                            types[livery.Type] = new List<string>();
-                                        types[livery.Type].Add(livery.Path);
-
-                                        if (!registrations.Contains(livery.Registration.ToLowerInvariant()))
-                                            registrations.Add(livery.Registration.ToLowerInvariant());
-                                        else
-                                            Console.WriteLine($"\tDuplicate registration found: {livery.Registration} in {livery.Path}");
-                                    }
+                                    Console.WriteLine("\t" + err);
                                 }
                             }
 
-                            Console.WriteLine("============Types============");
-                            foreach (var kvp in types)
+
+                            List<string> renameActions = RenameFolders(ref packages);
+                            if (renameActions.Count > 0)
                             {
-                                Console.WriteLine("\t" + kvp.Key);
-                                foreach (string path in kvp.Value)
+                                Console.WriteLine("============Rename Actions============");
+                                foreach (string action in renameActions)
                                 {
-                                    Console.WriteLine("\t\t" + path);
+                                    Console.WriteLine("\t" + action);
                                 }
                             }
-
                             break;
                         }
                 }
